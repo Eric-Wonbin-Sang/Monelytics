3da7d119ca0eccdccd4588133cf27a0663d1caa0
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-08-25 16:37:45 -0400

    Removed old test code for the venmo_account and bofa_account classes.

--- main.py
+++ main.py
@@ -1,28 +1,9 @@
-from AccountParsers import Account
-from StatementFiles import BofAStatement, VenmoStatement
+from Banks import BankOfAmerica
 
 
 def main():
 
-    bofa_personal_account = Account.Account(name="Personal", id="5329")
-    bofa_statement = BofAStatement.BofAStatement(
-        account=bofa_personal_account,
-        statement_path="C:\\Users\\ericw\\Desktop\\SourceFiles\\2020.12.01 - 2020.04.05 bofa 5329.csv"
-    )
-    bofa_personal_account.add_statement(bofa_statement)
+    BankOfAmerica.BankOfAmerica()
 
-    print(bofa_statement)
-
-    # ----------------------------------------------------------------------
-
-    venmo_account = Account.Account(name="Personal", id="5329")
-    venmo_statement = VenmoStatement.VenmoStatement(
-        account=venmo_account,
-        statement_path="C:\\Users\\ericw\\Desktop\\SourceFiles\\2020.01.16 - 2020.03.31 venmo.csv"
-    )
-    venmo_account.add_statement(venmo_statement)
-
-    print(venmo_account)
-    
 
 main()
