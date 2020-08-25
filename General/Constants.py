8b7bdadd3c0e07eaf134c7d2e1e960f45be4fa3d
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-08-25 16:36:39 -0400

    Added Constants file for keeping references to download directories, common chromedriver path, etc. Removed local chromedriver from project.

---
+++ General/Constants.py
@@ -0,0 +1,13 @@
+import os
+from General import Functions
+
+
+chrome_driver_path = Functions.get_curr_parent_dir("/API Keys/chromedriver.exe")
+
+bofa_login_credentials_json = Functions.get_curr_parent_dir("/API Keys/Monelytics/bofa - login credentials.json")
+bofa_login_cookies_pkl = Functions.get_curr_parent_dir("/API Keys/Monelytics/bofa - login cookies.pkl")
+bofa_accounts_download_dir = Functions.get_curr_parent_dir("/0 - Secrets/Monelytics/Bank of America")
+
+user_download_dir = "C:/Users/ericw/Downloads"
+
+project_dir = "/".join(os.path.dirname(os.path.realpath(__file__)).replace("\\", "/").split("/")[:-1])
