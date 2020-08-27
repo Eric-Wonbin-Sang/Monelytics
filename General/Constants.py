b1f70bfcfa1ec77775d276ad541b676364e5ea29
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-08-27 02:45:50 -0400

    Added the do_download cond for bank objects in Constants.py.

--- General/Constants.py
+++ General/Constants.py
@@ -10,6 +10,8 @@ bank_source_info_dir = Functions.get_curr_parent_dir("/0 - Secrets/Monelytics/Ba
 bofa_login_cookies_pkl = Functions.get_curr_parent_dir("/0 - Secrets/Monelytics/login cookies - bofa.pkl")
 venmo_login_cookies_pkl = Functions.get_curr_parent_dir("/0 - Secrets/Monelytics/login cookies - venmo.pkl")
 
+do_download = False
+
 user_download_dir = "C:/Users/ericw/Downloads"
 
 project_dir = "/".join(os.path.dirname(os.path.realpath(__file__)).replace("\\", "/").split("/")[:-1])
