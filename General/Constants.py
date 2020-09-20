5815ee82a5c0b09e51674fc8d84f133da553ae3a
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-09-20 16:56:26 -0400

    Removed chrome_driver_path and profile_list_json from Constants.py.

--- General/Constants.py
+++ General/Constants.py
@@ -2,13 +2,9 @@ import os
 from General import Functions
 
 
-chrome_driver_path = Functions.get_curr_parent_dir("/API Keys/chromedriver.exe")
-
-profile_list_json = Functions.get_curr_parent_dir("/0 - Secrets/Monelytics/profile_list.json")
-
 bank_source_info_dir = Functions.get_curr_parent_dir("/0 - Secrets/Monelytics/Bank Source Information")
 
-current_statement_file_name_default = "Current transactions.csv"
+current_statement_file_name_default = "Current transactions.csv"    # move to to-be-created account parent class
 
 user_download_dir = "C:/Users/ericw/Downloads"
 
