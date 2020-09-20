5c4f2a614ecc772e6fc4ce77529d77a1e45fae0e
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-09-20 17:03:49 -0400

    UpdateBot's get_driver function now uses bank_helper.

--- Banks/AbstractSystem/UpdateBot.py
+++ Banks/AbstractSystem/UpdateBot.py
@@ -1,12 +1,4 @@
-import pickle
-import os
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-
-from Banks.AbstractSystem import AbstractBank
-
-from General import Functions, Constants
+from Banks.AbstractSystem import AbstractBank, bank_helper
 
 
 class UpdateBot:
@@ -17,7 +9,7 @@ class UpdateBot:
         self.bank = AbstractBank.AbstractBank(bank_folder_dir=self.bank_folder_dir)
 
         self.cookies_path = self.bank_folder_dir + "/cookies.pkl"
-        self.login_url = Functions.parse_json(Constants.project_dir + "/Banks/bank_constants.json")[bank_type]
+        self.login_url = bank_helper.get_bank_info_dict(bank_type)["login_url"]
 
         self.driver = self.get_driver()
 
@@ -27,20 +19,12 @@ class UpdateBot:
         self.driver.close()
 
     def get_driver(self, detach=True, run_in_background=False):
-        options = Options()
-        options.add_argument("--app={}".format(self.login_url))
-        options.add_argument("window-size={},{}".format(1280, 1000))
-        options.add_experimental_option("detach", detach)
-
-        if run_in_background:
-            options.add_argument('--disable-gpu')
-            options.add_argument('--disable-software-rasterizer')
-
-        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-        if os.path.exists(self.cookies_path):
-            for cookie in pickle.load(open(self.cookies_path, "rb")):
-                driver.add_cookie(cookie)
-        return driver
+        return bank_helper.get_driver(
+            startup_url=self.login_url,
+            cookies_path=self.cookies_path,
+            detach=detach,
+            run_in_background=run_in_background
+        )
 
     def login(self):
         pass
