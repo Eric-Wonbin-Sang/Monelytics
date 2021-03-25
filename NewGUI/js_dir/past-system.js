fe9269d72925591d0bbb8be5fc42b7a82dc26079
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2021-03-25 15:00:48 -0400

    Added updated html, css, and javascript for past system page.

--- NewGUI/js_dir/past-system.js
+++ NewGUI/js_dir/past-system.js
@@ -1,49 +1,158 @@
 
+function get_p_elem(some_text) {
+    var p_elem = document.createElement("p");
+    var node = document.createTextNode(some_text);
+    p_elem.appendChild(node);
+    return p_elem;
+}
+
+class Bank {
+
+    constructor(bank_dict) {
+        this.type = bank_dict["type"];
+        this.owner = bank_dict["owner"];
+        this.username = bank_dict["username:"];
+        this.password = bank_dict["password"];
+        this.dict_list = bank_dict["account_list"];
+
+        this.account_list = this.get_account_list();
+        this.button = this.get_button();
+    }
+
+    get accounts_total() {
+        var total = 0;
+        for (let i in this.account_list) {
+            total += this.account_list[i].curr_balance;
+        }
+        return total;
+    }
+
+    get_account_list() {
+        var account_list = [];
+        for(let i_a in this.dict_list) {
+            account_list.push(new Account(this.dict_list[i_a]));
+        }
+        return account_list;
+    }
+
+    get_button() {
+        var button = document.createElement("button");
+        button.setAttribute("class", "collapsible");
+
+        var type_span = document.createElement("span");
+        type_span.setAttribute("class", "bank_type_span");
+        type_span.innerHTML = this.type + " (" + this.owner.split(" ")[0] + ")";
+
+        var total_span = document.createElement("span");
+        total_span.setAttribute("class", "bank_total_span");
+        
+        var total = this.accounts_total;
+        if (total == 0 & this.account_list.length == 0) {
+            total_span.innerHTML = "No Accounts!";
+        }
+        else {
+            total_span.innerHTML = "$" + new String(this.accounts_total);
+        }
+
+        button.appendChild(type_span);
+        button.appendChild(total_span);
+
+        button.onclick = function() {
+
+            var account_div = this.nextElementSibling;
+            if (account_div != null) {
+
+                button.classList.toggle("active");
+
+                if (account_div.style.maxHeight){
+                    account_div.style.maxHeight = null;
+                } else {
+                    account_div.style.maxHeight = account_div.scrollHeight + "px";
+                } 
+            }
+        };
+
+        return button;
+    }
+
+    get_div() {
+        var bank_div = document.createElement("div");
+        bank_div.setAttribute("class", "bank_div");
+
+        if (this.dict_list.length == 0) {
+            var account_div = document.createElement("div");
+            account_div.setAttribute("class", "account_div");
+            account_div.setAttribute("id", 0);
+            account_div.appendChild(get_p_elem("No Accounts!"));
+            bank_div.appendChild(account_div);
+        }
+
+        bank_div.appendChild(this.button);
+
+        for(let i_a in this.account_list) {
+            bank_div.appendChild(this.account_list[i_a].get_div());
+        }
+        return bank_div;
+    }
+
+}
+
+class Account {
+
+    constructor(account_dict) {
+        this.name = account_dict["name"];
+        this.nickname = account_dict["nickname"];
+        this.type = account_dict["type"];
+        this.specific_type = account_dict["specific_type"];
+        this.curr_balance = account_dict["curr_balance"];
+    }
+
+    get_div() {
+        var account_div = document.createElement("div");
+        account_div.setAttribute("class", "account_div");
+
+        account_div.appendChild(get_p_elem("Account: " + this.name));
+        account_div.appendChild(get_p_elem("Nickname: " + this.nickname));
+        account_div.appendChild(get_p_elem("Type: " + this.type));
+        account_div.appendChild(get_p_elem("Spec Type: " + this.specific_type));
+        
+        return account_div;
+    }
+}
+
+function get_bank_list(json_response) {
+    var bank_list = [];
+    var bank_dict_list = JSON.parse(json_response);
+    for(let i_b in bank_dict_list){
+        bank_list.push(new Bank(bank_dict_list[i_b]));
+    }
+    return bank_list;
+}
+
+function get_banks_and_accounts_sidebar_div(bank_list) {
+    var banks_and_accounts_sidebar_div = document.getElementById("banks_and_accounts_sidebar");
+    for(let i in bank_list){
+        banks_and_accounts_sidebar_div.appendChild(bank_list[i].get_div());
+    }
+}
+
 window.onload = function() {
-    create_banks_and_accounts();
-};
-
-function create_banks_and_accounts() {
-
     var url = "http://127.0.0.1:5000/get_bank_and_account_info";
-
-    const request = new XMLHttpRequest();
+    request = new XMLHttpRequest();
     request.open("GET", url);
     request.send();
-
     request.onload = (e) => {
 
-        var bank_dick_list = JSON.parse(request.response);
+        var json_response = request.response
+        var bank_list = get_bank_list(json_response);
 
-        for(let i_b in bank_dick_list){
+        get_banks_and_accounts_sidebar_div(bank_list);
 
-            var bank_type = bank_dick_list[i_b]["type"]
-            var bank_owner = bank_dick_list[i_b]["owner"]
-            var bank_username = bank_dick_list[i_b]["username:"]
-            var bank_password = bank_dick_list[i_b]["password"]
-            var account_dict_list = bank_dick_list[i_b]["account_list"]
-
-            console.log(bank_type);
-            console.log(bank_owner);
-            console.log(bank_username);
-            console.log(bank_password);
-
-            for(let i_a in account_dict_list){
-
-                var account_type = account_dict_list[i_a]["type"]
-                var account_name = account_dict_list[i_a]["name"]
-                var account_nickname = account_dict_list[i_a]["nickname"]
-                var account_specific_type = account_dict_list[i_a]["specific_type"]
-                var account_curr_balance = account_dict_list[i_a]["curr_balance"]
-
-                console.log("\t", account_type);
-                console.log("\t", account_name);
-                console.log("\t", account_nickname);
-                console.log("\t", account_specific_type);
-                console.log("\t", account_curr_balance);
+        for (let i in bank_list) {
+            bank_list[i].button.click();
+            if (i == 0) {
+                break;
             }
-            console.log("--------------");
-
         }
     }
     request.onerror = (e) => {
