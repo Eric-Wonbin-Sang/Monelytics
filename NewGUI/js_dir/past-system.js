6b47c2efe7e9097a87884c61bc7d691620dd24ee
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2021-03-28 14:27:48 -0400

    Added updated html, css, and javascript for past system page.

--- NewGUI/js_dir/past-system.js
+++ NewGUI/js_dir/past-system.js
@@ -6,6 +6,13 @@ function get_p_elem(some_text) {
     return p_elem;
 }
 
+function get_dollar_str(num) {
+    if (num < 0) {
+        return "-$" + num.toLocaleString().substring(1);
+    }
+    return "$" + num.toLocaleString();
+}
+
 class Bank {
 
     constructor(bank_dict) {
@@ -30,14 +37,14 @@ class Bank {
     get_account_list() {
         var account_list = [];
         for(let i_a in this.dict_list) {
-            account_list.push(new Account(this.dict_list[i_a]));
+            account_list.push(new Account(i_a, this.dict_list[i_a]));
         }
         return account_list;
     }
 
     get_button() {
         var button = document.createElement("button");
-        button.setAttribute("class", "collapsible");
+        button.setAttribute("class", "bank_collapsible");
 
         var type_span = document.createElement("span");
         type_span.setAttribute("class", "bank_type_span");
@@ -51,7 +58,7 @@ class Bank {
             total_span.innerHTML = "No Accounts!";
         }
         else {
-            total_span.innerHTML = "$" + new String(this.accounts_total);
+            total_span.innerHTML = get_dollar_str(this.accounts_total);
         }
 
         button.appendChild(type_span);
@@ -60,7 +67,7 @@ class Bank {
         button.onclick = function() {
 
             var account_div = this.nextElementSibling;
-            if (account_div != null) {
+            if (account_div.innerHTML != "") {
 
                 button.classList.toggle("active");
 
@@ -78,28 +85,26 @@ class Bank {
     get_div() {
         var bank_div = document.createElement("div");
         bank_div.setAttribute("class", "bank_div");
-
-        if (this.dict_list.length == 0) {
-            var account_div = document.createElement("div");
-            account_div.setAttribute("class", "account_div");
-            account_div.setAttribute("id", 0);
-            account_div.appendChild(get_p_elem("No Accounts!"));
-            bank_div.appendChild(account_div);
-        }
-
         bank_div.appendChild(this.button);
 
+        var account_list_div = document.createElement("div");
+        account_list_div.setAttribute("class", "all_accounts_div");
+
         for(let i_a in this.account_list) {
-            bank_div.appendChild(this.account_list[i_a].get_div());
+            account_list_div.appendChild(this.account_list[i_a].get_div());
         }
+
+        bank_div.appendChild(account_list_div);
         return bank_div;
     }
-
 }
 
 class Account {
 
-    constructor(account_dict) {
+    constructor(index, account_dict) {
+
+        this.index = index;
+
         this.name = account_dict["name"];
         this.nickname = account_dict["nickname"];
         this.type = account_dict["type"];
@@ -107,15 +112,46 @@ class Account {
         this.curr_balance = account_dict["curr_balance"];
     }
 
+    get_button() {
+        var button = document.createElement("button");
+        button.setAttribute("class", "account_button");
+
+        var name_span = document.createElement("span");
+        name_span.setAttribute("class", "account_name_span");
+        name_span.innerHTML = this.name;
+        button.appendChild(name_span);
+
+        var amount_span = document.createElement("span");
+        amount_span.setAttribute("class", "account_amount_span");
+        amount_span.innerHTML = get_dollar_str(this.curr_balance);
+        button.appendChild(amount_span);
+
+        button.onclick = function() {
+            
+        };
+
+        return button;
+    }
+
     get_div() {
         var account_div = document.createElement("div");
         account_div.setAttribute("class", "account_div");
 
-        account_div.appendChild(get_p_elem("Account: " + this.name));
-        account_div.appendChild(get_p_elem("Nickname: " + this.nickname));
-        account_div.appendChild(get_p_elem("Type: " + this.type));
-        account_div.appendChild(get_p_elem("Spec Type: " + this.specific_type));
+        account_div.appendChild(this.get_button());
+
+        // account_div.appendChild(get_p_elem("Account: " + this.name));
+        // account_div.appendChild(get_p_elem("Nickname: " + this.nickname));
+        // account_div.appendChild(get_p_elem("Type: " + this.type));
+        // account_div.appendChild(get_p_elem("Spec Type: " + this.specific_type));
+        // account_div.appendChild(get_p_elem("Balance: " + get_dollar_str(this.curr_balance)));
         
+        // var temp = this.name;
+
+        // account_div.onclick = function() {
+        //     console.log(temp);
+        //     console.log(this.name);
+        // };
+
         return account_div;
     }
 }
